f57b25d70fde1e228d5243fcc2babab814649875
Michael Bironneau <michael.bironneau@openenergi.com>
2018-05-18 20:42:02 +0100

    Fix serialization of grill temp readings

--- data.py
+++ data.py
@@ -79,7 +79,7 @@ class eventhub_sender():
             self.redis.lpush('readings', reading_meat)
         reading_grill = self.get_reading(self.grill_sensor, "grill")
         if reading_grill:
-            self.redis.lpush('readings', json.dumps(reading_grill))
+            self.redis.lpush('readings', reading_grill)
         
     def send(self):
         # Check if last good transmission is over 120s ago, try to reconnect if so
