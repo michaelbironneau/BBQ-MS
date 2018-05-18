6df1124d03c14901251bf3252502a36cc5e888f3
Michael Bironneau <michael.bironneau@openenergi.com>
2018-05-18 18:49:57 +0100

    Implement data upload to DD2 platform

--- main.py
+++ main.py
@@ -1,94 +1,52 @@
 #!/usr/bin/python
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
 # Can enable debug output by uncommenting:
 #import logging
 #logging.basicConfig(level=logging.DEBUG)
 
 import time
 import math
-
+import sys 
+import json
+from data import eventhub_sender
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_GPIO.MAX31855 as MAX31855
-
-
-# Define a function to convert celsius to fahrenheit.
-def c_to_f(c):
-        return c * 9.0 / 5.0 + 32.0
-
-
-# Uncomment one of the blocks of code below to configure your Pi or BBB to use
-# software or hardware SPI.
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 # Raspberry Pi software SPI configuration.
 CLK = 25
 CS  = 24
 DO  = 18
-sensor = MAX31855.MAX31855(CLK, CS, DO)
+grill_sensor = MAX31855.MAX31855(CLK, CS, DO)
 
 # Sensor 2
 CLK_2 = 22
 DO_2 = 17
 CS_2 = 27
-sensor_2 = MAX31855.MAX31855(CLK_2, CS_2, DO_2)
+meat_sensor = MAX31855.MAX31855(CLK_2, CS_2, DO_2)
 
-# Raspberry Pi hardware SPI configuration.
-#SPI_PORT   = 0
-#SPI_DEVICE = 0
-#sensor = MAX31855.MAX31855(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+if __name__ == '__main__':
+    # Load settings file
+    settings_file = sys.argv[-1]
+    with open(settings_file) as f:
+        settings = json.load(f)
 
-# BeagleBone Black software SPI configuration.
-#CLK = 'P9_12'
-#CS  = 'P9_15'
-#DO  = 'P9_23'
-#sensor = MAX31855.MAX31855(CLK, CS, DO)
-
-# BeagleBone Black hardware SPI configuration.
-#SPI_PORT   = 1
-#SPI_DEVICE = 0
-#sensor = MAX31855.MAX31855(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
-
-# Loop printing measurements every second.
-print('Press Ctrl-C to quit.')
-while True:
-    temp_1 = sensor.readTempC()
-    internal_1 = sensor.readInternalC()
-    
-    temp_2  = sensor_2.readTempC()
-    internal_2 = sensor_2.readInternalC()
-    
-    t1 = float(temp_1)
-    if math.isnan(t1):
-        print('Sensor @(25,24,18): Internal {0:0.3F}*C | Thermocouple {1}'.format(internal_1, temp_1))
-    else:    
-        print('Sensor @(25,24,18): Internal {0:0.3F}*C | Thermocouple {1:0.3F}*C'.format(internal_1, temp_1))
-        
-
-    t2 = float(temp_2)
-    if math.isnan(t2):
-        print('Sensor @(22,27,17): Internal {0:0.3F}*C | Thermocouple {1}'.format(internal_2, temp_2))
-    else:    
-        print('Sensor @(22,27,17): Internal {0:0.3F}*C | Thermocouple {1:0.3F}*C'.format(internal_2, temp_2))
-   
-    #print('Thermocouple Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, c_to_f(temp)))
-    #print('    Internal Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(internal, c_to_f(internal)))
-    time.sleep(1.0)
+    # Set up scheduler
+    scheduler = BlockingScheduler()
+    # Class to send data
+    sender = eventhub_sender(grill_sensor, meat_sensor,
+                             os.getenv('IOT_HOST', settings.get('iot-host', 'oeiot.azure-devices.net')),
+                             os.getenv('DEVICE_ID', settings.get('device-id', '')),
+                             os.getenv('SHARED_KEY', settings.get('shared-key', '')),
+                             os.getenv('PROTOCOL', settings.get('protocol', 'mqtt')))
+    scheduler.add_job(sender.read,
+                      'interval', seconds=settings.get('read-interval', 1),
+                      id='read_data', coalesce=True)
+    scheduler.add_job(sender.send,
+                      'interval', seconds=settings.get('data-send-interval', 1),
+                      id='send_data', coalesce=True)
+    scheduler.add_job(sender.retry_failed, 'interval', seconds=120,
+                      id='retry_failed', coalesce=True)
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
